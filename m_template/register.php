c60703479895361a4047e7689df9b5de21189fe2
Gabriel Ferrer Torres <gxferrertorres@gmail.com>
2020-05-10 15:07:44 -0400

    New index, registration, css
    Le añadi nuevo css a las paginas de index y registracion de nuevo estudiante. Tambien puse nuevo metodo en el documento de css

--- m_template/register.php
+++ m_template/register.php
@@ -12,6 +12,61 @@ session_start();
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/mobile.css" media="screen and (max-width : 568px)">
 	<script type="text/javascript" src="js/mobile.js"></script>
+    <style>
+    
+    .input_box{
+       padding:10px;
+       font-size: 14px;
+       border: 1px solid gray;
+       border-radius: 5px;
+    }
+    /*Letter size*/
+    .td_letter{
+        font-size:18px;
+        padding-right: 16px;
+    }
+    /*Button style*/
+    .b1{
+    color:white;
+    background-color:blue;
+    font-size: 20px;
+    background-image: linear-gradient(to bottom right, aqua, gray);
+    padding: 9px;
+    border: none;
+    border-radius: 3px;
+    }
+    .b1:hover{
+    color:white;
+    background-color:darkgray;
+    background-image: linear-gradient(to bottom right, aqua, black);
+    padding: 9px;
+    border: none;
+    border-radius: 3px;
+    }
+    /* a LINKS*/
+    .link_a{
+    color: #252525;
+	display: inline-block;
+	font-family: "Arial Black", Gadget, sans-serif;
+	font-size: 14px;
+	font-weight: normal;
+	margin: 0;
+	padding: 0 0 3px;
+	text-decoration: none;
+	text-transform: uppercase;
+    }
+    .link_a:hover{
+    color: #0ba39c;
+	display: inline-block;
+	font-family: "Arial Black", Gadget, sans-serif;
+	font-size: 14px;
+	font-weight: normal;
+	margin: 0;
+	padding: 0 0 3px;
+	text-decoration: none;
+	text-transform: uppercase;
+    }
+    </style>
 </head>
 <body>
 <div id="header">
@@ -133,9 +188,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
                             else//VISITANTE
                             {//Usuario no es estudiante (NO existe en tabla estudiante2)
                                 print '<h3>Solo estudiantes pueden registrarse!</h3>';
-                                print '<h3>Si es estudiante, asegúrese entrar su email correctamente. Vuelva a intentarlo...<br /><a href="register.php"> Registrar </a></h3>';
+                                print '<h3>Si es estudiante, asegúrese entrar su email correctamente. Vuelva a intentarlo...<br /><a href="register.php" class="link_a"> Registrar </a></h3>';
                                 print '<h3>Para ver los estudiantes de honor como visitante oprima
-                                <a href="estudiantes.php">Aquí</a></h3>';
+                                <a class="link_a" href="estudiantes.php">Aquí</a></h3>';
                             }
 
                 }
@@ -148,7 +203,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
         // No entró uno de los campos
 
-        print '<h3>Asegúrese de entrar su email y password. Vuelva a intentarlo...<br /><a href="register.php"> Registrar </a></h3>';
+        print '<h3>Asegúrese de entrar su email y password. Vuelva a intentarlo...<br /><a class="link_a" href="register.php"> Registrar </a></h3>';
 
 
 
@@ -171,44 +226,44 @@ print '<div id="container"><center>
   <table id="table2" width="349" border="0">
   
     <tr>
-      <td align="right">E-mail: </td>
+      <td class="td_letter" align="right">*E-mail: </td>
       <td align="left"><label for="email"></label>
-      <input type="email" name="email" id="email" value="'.$_SESSION['email'].'" required/>
-      <span class="error">*</span>
+      <input class="input_box" type="email" name="email" id="email" value="'.$_SESSION['email'].'" required/>
+      <span class="error"></span>
       </td>
     </tr>
     
     <tr>
-      <td align="right">Password: </td>
+      <td class="td_letter" align="right">*Password: </td>
       <td align="left"><label for="password"></label>
-      <input type="password" name="password" id="password" required/>
-      <span class="error">*</span>
+      <input class="input_box" type="password" name="password" id="password" required/>
+      <span class="error"></span>
       </td>
     </tr>
     
     <tr>
     <span class="error">'.$_SESSION['err_mess'].'</span>
-      <td align="right">Confirm Password: </td>
-      <td align="left"><label for="con_pass"></label>
-      <input type="password" name="con_pass" id="con_pass" required/>
-      <span class="error">*</span>
+      <td class="td_letter" align="right">*Confirm Password: </td>
+      <td align="center"><label for="con_pass"></label>
+      <input class="input_box" type="password" name="con_pass" id="con_pass" required/>
+      <span class="error"></span>
       </td>
     </tr>
     
     <tr>
-      <td align="right">Telefono: </td>
+      <td class="td_letter" align="right">Telefono: </td>
       <td align="left"><label for="telefono"></label>
-      <input type="tel" name="telefono" id="telefono" value="'.$_SESSION['tel'].'"/>
+      <input class="input_box" type="tel" name="telefono" id="telefono" value="'.$_SESSION['tel'].'"/>
       </td>
     </tr>
       
     <tr>
-      <td colspan="2" align="center"><input type="submit" name="submit" id="submit" value="Registrar"/></td>
+      <td colspan="2" align="center"><input class="b1" type="submit" name="submit" id="submit" value="Registrar"/></td>
       </tr>
   </table>
 </form>
 <br>
-<button><a href="index.php" id="botton"> Regresar </a></button>
+<a class="link_a" href="index.php" id="botton"> Regresar </a>
 
 </center></div>';			  
 }
