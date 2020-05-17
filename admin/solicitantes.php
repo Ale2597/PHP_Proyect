aa879cbe2216390c66cd3ac4ae2c74c9d71ac1d6
Alejandro Zeno <alejandro.zeno@upr.edu>
2020-05-17 19:56:45 -0400

    Solicitantes Completado

--- admin/solicitantes.php
+++ admin/solicitantes.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <!-- Website Template by freewebsitetemplates.com -->
 <?php 
+include('../conectiondb.php');
 //Empezar Sesion.
 session_start();
 ?>
@@ -12,17 +13,38 @@ session_start();
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
 	<link rel="stylesheet" type="text/css" href="../css/mobile.css" media="screen and (max-width : 568px)">
 	<script type="text/javascript" src="../js/mobile.js"></script>
+	
+    <style>
+        #table1{
+            background-color: azure;
+            border-bottom: collapse;
+            width: auto;
+        }
+
+        #table_header{
+            text-align:center;
+            background-color:darkkhaki;
+        }
+
+        #table_rows{
+            text-align:center;
+        }
+        td{
+            width: 300px;
+            height: 50px;
+        }
+    </style>
 </head>
 <body>
 	<div id="header">
 		<a href="index.html" class="logo">
-			<img src="../images/logo.jpg" alt="">
+			<img src="../images/Logo_icon.PNG" alt="">
 		</a>
 		<ul id="navigation">
-			<li class="selected">
-				<a href="index.php">home</a>
+			 <li>
+				<a href="index.php">Inicio</a>
 			</li>
-			<li>
+			<li class="selected">
 				<a href="solicitantes.php">Solicitantes</a>
 			</li>
 			<li>
@@ -38,40 +60,67 @@ session_start();
 				<a href="../logout.php">Logout</a>
 			</li>
 		</ul>
+		
+		<h1>Solicitudes</h1>
 	</div>
 	<div id="body">
-		<div id="featured">
-			<img src="../images/the-beacon.jpg" alt="">
-			<div>
-			    <h2> Welcome Admin <?php echo $_SESSION['nombre_admin']; ?>! </h2>
-				<h2>the beacon to all mankind</h2>
-				<span>Our website templates are created with</span>
-				<span>inspiration, checked for quality and originality</span>
-				<span>and meticulously sliced and coded.</span>
-				<a href="#" class="more">read more</a>
-			</div>
-		</div>
+		
+<!--
 		<ul>
 			<li>
 				<a href="#">
 					<img src="../images/the-father.jpg" alt="">
-					<span>Gabriel Ferrer</span>
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<img src="../images/the-actor.jpg" alt="">
 					<span>Hiram Vera</span>
 				</a>
 			</li>
 			<li>
 				<a href="#">
-					<img src="../images/the-nerd.jpg" alt="">
+					<img src="../images/the-actor.jpg" alt="">
 					<span>Alejandro Zeno</span>
 				</a>
 			</li>
+			<li>
+				<a href="#">
+					<img src="../images/the-nerd.jpg" alt="">
+					<span>Gabriel Ferrer</span>
+				</a>
+			</li>
 		</ul>
-	</div>
+-->
+
+
+    <?php 
+            $query = "SELECT nombre, apellido1, apellido2, nombre_depto, nombre_beca, costo
+                      FROM (usuarios NATURAL JOIN estudiantes) 
+                      JOIN solicitud USING (user_id) 
+                      JOIN depto USING (depto_id)
+                      JOIN becas USING (beca_id)";
+
+            if($r = mysqli_query($dbc, $query))
+            {
+                print"<div><center><table id='table1'>";
+                print"<tr id='table_header'>
+                        <td><b>Nombre Estudiante</b></td>
+                        <td><b>Departamento</b></td>
+                        <td><b>Beca</b></td>
+                        <td><b>Cantidad Solicitada</b></td>
+                        </tr>";
+
+                while($row=mysqli_fetch_array($r))
+                {
+                    print"<tr id='table_rows'>
+                        <td>$row[nombre] $row[apellido1] $row[apellido2]</td>
+                        <td>$row[nombre_depto]</td>
+                        <td>$row[nombre_beca]</td>
+                        <td>$row[costo]</td>
+                        </tr>";
+                }
+
+                print"</table></center></div><br>";
+            }
+        ?>
+        </div>
+	
 	<div id="footer">
 		<div>
 			<p>&copy; 2023 by Mustacchio. All rights reserved.</p>
