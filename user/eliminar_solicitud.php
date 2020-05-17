f84e0a49e3e20998a39f237bfa8be665d2fd0efb
HughesHoenheim <42414879+HughesHoenheim@users.noreply.github.com>
2020-05-17 00:28:18 -0400

    eliminar solicitud

--- user/eliminar_solicitud.php
+++ user/eliminar_solicitud.php
@@ -32,6 +32,26 @@ session_start();
             width: 300px;
             height: 50px;
         }
+        table h3{
+        color:cadetblue;
+        }
+        .b1{
+        color:white;
+        background-color:blue;
+        font-size: 20px;
+        background-image: linear-gradient(to bottom right, aqua, gray);
+        padding: 9px;
+        border: none;
+        border-radius: 3px;
+        }
+        .b1:hover{
+        color:white;
+        background-color:darkgray;
+        background-image: linear-gradient(to bottom right, aqua, black);
+        padding: 9px;
+        border: none;
+        border-radius: 3px;
+        }
     </style>
 </head>
 <body>
@@ -92,23 +112,25 @@ session_start();
 		</ul>
 -->
         <div>
-        <?php 
-            $query = "SELECT *
-            FROM solicitud
-            WHERE user_id={$_SESSION['user_id']}";
-
-            if($r = mysqli_query($dbc, $query))
+        <?php
+       
+            if(isset($_GET['sol_id']) && is_numeric($_GET['sol_id']))
             {
+                $query = "SELECT *
+                FROM solicitud
+                WHERE sol_id={$_GET['sol_id']}";
+                
+                if($r = mysqli_query($dbc, $query))
+                {
+                print '<form action="eliminar_solicitud.php" method="post">';
                 print"<div><center><table id='table1'>";
-                print"<h2>Mis Solicitudes</h2>";
+                print"<h2>Esta Seguro que desea eliminar la siguiente Solicitud?</h2>";
                 print"<tr id='table_header'>
                         <td><b>Nombre de Actividad</b></td>
                         <td><b>Costo</b></td>
                         <td><b>Fecha de Actividad</b></td>
                         <td><b>Fecha de Solicitud</b></td>
                         <td><b>Status</b></td>
-						<td><b>Editar</b></td>
-						<td><b>Eliminar</b></td>
                         </tr>";
 
                 while($row=mysqli_fetch_array($r))
@@ -119,15 +141,42 @@ session_start();
                         <td>$row[fecha_actividad]</td>
                         <td>$row[fecha_sol]</td>
                         <td>$row[status]</td>
-                        <td><a href=\"editar_solicitud.php?sol_id={$row['sol_id']}\">
-						<img width='25px' height='25px' alt=\"Edit\" src=\"../images/edit_icon.png\"></a></td>
-						<td><a href=\"eliminar_solicitud.php?sol_id={$row['sol_id']}\">
-                        <img width='25px' height='25px' alt=\"Edit\" src=\"../images/close_icon.png\"></a></td>
                         </tr>";
+
+                        
                 }
 
                 print"</table></center></div><br>";
+                print '<input type="hidden" name="sol_id" value="'.$_GET['sol_id'].'">
+                <input type="submit" name="submit" value="Eliminar Solicitud">';
+                print"</form>";
+                }            
+                
+                else
+                {
+                    print '<p style="color:red;">No se pudo mostrar al estudiante que se desea eliminar porque: 
+                    <br/>'.mysqli_error($dbc).'.</p>';
+                }
             }
+            elseif(isset($_POST['sol_id']) && is_numeric($_POST['sol_id']))
+            {
+                $query = "DELETE FROM solicitud WHERE sol_id={$_POST['sol_id']} LIMIT 1";
+                $r = mysqli_query($dbc, $query);
+                
+                if(mysqli_affected_rows($dbc) == 1)
+                {
+                    echo "<script>alert('La Solicitud ha sido eliminada con exito!!')</script>";
+                    echo "<script>window.open('./solicitudes_usuario.php','_self')</script>";
+                }
+                else
+                    print '<p style="color:red;">No se pudo eliminar al estudiante porque: 
+                <br/>'.mysqli_error($dbc).'.</p>';
+            }
+            else
+                print '<p style="color:red;">Esta pagina ha sido accedida con error!</p>';
+            
+            mysqli_close($dbc);
+        
         ?>
         </div>
 	</div>
